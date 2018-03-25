ea6703216c49cafda56d25c96193b768aa801b01
ShawnXie <shawn0326@163.com>
2018-03-25 23:00:36 +0800

    fix bug

--- src/core/render/shader/shaderChunk/viewModelPos_pars_vert.glsl
+++ src/core/render/shader/shaderChunk/viewModelPos_pars_vert.glsl
@@ -1,3 +1,3 @@
-#if defined(USE_POINT_LIGHT) || defined(USE_SPOT_LIGHT) || defined(USE_NORMAL_MAP) || defined(USE_BUMPMAP) || defined(FLAT_SHADED) || (defined(USE_PHONG) && defined(USE_DIRECT_LIGHT)) || (defined(USE_PBR) && defined(USE_DIRECT_LIGHT)) || defined(NUM_CLIPPING_PLANES)
+#if defined(USE_POINT_LIGHT) || defined(USE_SPOT_LIGHT) || defined(USE_NORMAL_MAP) || defined(USE_BUMPMAP) || defined(FLAT_SHADED) || defined(USE_PHONG) || defined(USE_PBR)|| defined(NUM_CLIPPING_PLANES)
     varying vec3 v_ViewModelPos;
 #endif
