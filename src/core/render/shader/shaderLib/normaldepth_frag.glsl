78a2a6fbfce2f36b41c2229674e820a7a6633cd0
ShawnXie <shawn0326@163.com>
2018-05-07 14:21:22 +0800

    normal depth shader support alpha ignore

--- src/core/render/shader/shaderLib/normaldepth_frag.glsl
+++ src/core/render/shader/shaderLib/normaldepth_frag.glsl
@@ -1,4 +1,7 @@
 #include <common_frag>
+#include <diffuseMap_pars_frag>
+
+#include <uv_pars_frag>
 
 #define USE_NORMAL
 
@@ -6,6 +9,14 @@
 #include <normal_pars_frag>
 
 void main() {
+    #ifdef USE_DIFFUSE_MAP
+        vec4 texelColor = texture2D( texture, v_Uv );
+
+        float alpha = texelColor.a * u_Opacity;
+        if(alpha < 0.99) { // if transparent, ignore
+            discard;
+        }
+    #endif
     vec4 packedNormalDepth;
     packedNormalDepth.xyz = normalize(v_Normal) * 0.5 + 0.5;
     packedNormalDepth.w = gl_FragCoord.z;
--- src/core/render/shader/shaderLib/normaldepth_vert.glsl
+++ src/core/render/shader/shaderLib/normaldepth_vert.glsl
@@ -4,7 +4,9 @@
 
 #include <skinning_pars_vert>
 #include <normal_pars_vert>
+#include <uv_pars_vert>
 void main() {
+    #include <uv_vert>
     #include <begin_vert>
     #include <skinning_vert>
     #include <normal_vert>
