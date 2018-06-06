b3d544d813827d4673bfcabcd5e4971ae7aa3c17
shawn <shawn0326@163.com>
2018-06-06 23:35:22 +0800

    depth shader: add alpha test

--- src/core/render/shader/shaderLib/depth_vert.glsl
+++ src/core/render/shader/shaderLib/depth_vert.glsl
@@ -1,6 +1,8 @@
 #include <common_vert>
 #include <skinning_pars_vert>
+#include <uv_pars_vert>
 void main() {
+    #include <uv_vert>
     #include <begin_vert>
     #include <skinning_vert>
     #include <pvm_vert>
