1402e2a6292f1f9a8dbf399934f1e0fa106c51b2
ShawnXie <shawn0326@163.com>
2019-08-25 13:39:29 +0800

    remove zen3d.vox.js

--- src/extension/vox/main.js
+++
@@ -1,7 +0,0 @@
-import {VOXLoader} from './VOXLoader.js';
-import {VOXMeshBuilder} from './VOXMeshBuilder.js';
-
-zen3d = zen3d || {};
-
-zen3d.VOXLoader = VOXLoader;
-zen3d.VOXMeshBuilder = VOXMeshBuilder;
